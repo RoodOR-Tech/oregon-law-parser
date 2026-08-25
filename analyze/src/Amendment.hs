6fff01c1438641cf3ce212390339fe00ea54fc40
RoodOR-Tech <roodj.or@gmail.com>
2026-08-24 19:27:26 -0700

    Replace parser exceptions with structured results

--- analyze/src/Amendment.hs
+++ analyze/src/Amendment.hs
@@ -5,11 +5,14 @@ module Amendment where
 import           Data.Aeson        (ToJSON)
 import           Data.Function     ((&))
 import           Data.List         (isInfixOf, isPrefixOf, isSubsequenceOf, nub, sort)
+import           Data.Maybe        (isNothing)
 import           Data.String.Utils (split, splitWs)
-import           Data.Time         (Day, defaultTimeLocale, parseTimeOrError)
+import           Data.Time         (Day, defaultTimeLocale, parseTimeM)
 import           GHC.Generics
 import           Prelude.Unicode
+import           Provenance
 import           StringOps
+import           Text.Read         (readMaybe)
 import           Text.Regex.TDFA
 
 data Amendment =
@@ -17,10 +20,11 @@ data Amendment =
     affectedSections ∷ ChangeSet,
     bill             ∷ Bill,
     chapter          ∷ Integer,
-    summary          ∷ String,
+    summary          ∷ Maybe String,
     effectiveDate    ∷ Day,
     year             ∷ Integer,
-    validation       ∷ Validation
+    validation       ∷ Validation,
+    provenance       ∷ Provenance
   } deriving (Show, Generic)
 
 data ChangeSet =
@@ -44,6 +48,22 @@ data Validation =
     bodySections     ∷ ChangeSet
   } deriving (Eq, Show, Generic)
 
+data ParseErrorCode
+  = MissingCitation
+  | InvalidCitation
+  | MissingYear
+  | MissingChapter
+  | MissingEffectiveDate
+  | ExtractionFailed
+  deriving (Eq, Show, Generic)
+
+data ParseError =
+  ParseError {
+    parseErrorCode    ∷ ParseErrorCode,
+    parseErrorField   ∷ Maybe String,
+    parseErrorMessage ∷ String
+  } deriving (Eq, Show, Generic)
+
 type SectionNumber = String
 
 data Bill =
@@ -61,64 +81,57 @@ instance ToJSON BillType
 instance ToJSON ChangeSet
 instance ToJSON Validation
 instance ToJSON ValidationStatus
+instance ToJSON ParseError
+instance ToJSON ParseErrorCode
 
 emptyChangeSet ∷ ChangeSet
 emptyChangeSet = ChangeSet { amended = [], repealed = [] }
 
-makeBill ∷ String → Bill
+makeBill ∷ String → Maybe Bill
 makeBill citation =
-  let [chamber, number] = splitWs citation
-  in  Bill { billType = read chamber, billNumber = read number }
+  case splitWs citation of
+    [chamber, number] -> do
+      parsedType ← readMaybe chamber
+      parsedNumber ← readMaybe number
+      pure Bill { billType = parsedType, billNumber = parsedNumber }
+    _ -> Nothing
 
-findCitation ∷ [String] → String
+findCitation ∷ [String] → Maybe String
 findCitation phrases =
-  let maybeMatch = phrases
-        & join
-        & firstMatch "(HB|SB) [0-9]+"
-  in case maybeMatch of
-    Just s -> s
-    Nothing -> error "Could not find a citation"
+  phrases
+    & join
+    & firstMatch "(HB|SB) [0-9]+"
 
-findYear ∷ [String] → Integer
-findYear input =
-  let maybeMatch = input
-        & join
-        & firstMatch "OREGON LAWS [0-9]{4}"
-  in case maybeMatch of
-    Just s -> s
-              & splitWs
-              & last
-              & read
-    Nothing -> error "Could not find the year"
+findYear ∷ [String] → Maybe Integer
+findYear input = do
+  matched ← input
+    & join
+    & firstMatch "OREGON LAWS [0-9]{4}"
+  case splitWs matched of
+    [] -> Nothing
+    xs -> readMaybe (last xs)
 
-findChapter ∷ [String] → Integer
-findChapter phrases =
-  let maybeMatch =
-        phrases
-        & join
-        & firstMatch "Chap. [0-9]{1,3}"
-  in case maybeMatch of
-    Just s -> s
-              & splitWs
-              & last
-              & read
-    Nothing -> error "Could not find the Chapter"
+findChapter ∷ [String] → Maybe Integer
+findChapter phrases = do
+  matched ← phrases
+    & join
+    & firstMatch "Chap. [0-9]{1,3}"
+  case splitWs matched of
+    [] -> Nothing
+    xs -> readMaybe (last xs)
 
-findEffectiveDate ∷ [String] → Day
-findEffectiveDate paragraphs =
-  let maybeMatch =
-        paragraphs
-        & join
-        & firstMatch "Effective date .+ [0-9]+, [0-9]{4}"
-  in case maybeMatch of
-    Just s -> parseTimeOrError True defaultTimeLocale "Effective date %B %-d, %Y" s
-    Nothing -> error "Could not find the Effective Date"
+findEffectiveDate ∷ [String] → Maybe Day
+findEffectiveDate input = do
+  matched ← input
+    & join
+    & firstMatch "Effective date .+ [0-9]+, [0-9]{4}"
+  parseTimeM True defaultTimeLocale "Effective date %B %-d, %Y" matched
 
-findSummary ∷ [String] → String
+findSummary ∷ [String] → Maybe String
 findSummary phrases =
   case filter isSummary phrases of
-    [aSummary] → cleanUp aSummary
-    _          → "(Summary is not available)"
+    [aSummary] → Just (cleanUp aSummary)
+    _          → Nothing
 
 isSummary ∷ String → Bool
 isSummary sentence =
@@ -205,6 +218,48 @@ selectBestChangeSet ∷ ChangeSet → ChangeSet → ChangeSet
 selectBestChangeSet titleChanges bodyChanges
   | bodyChanges == emptyChangeSet = titleChanges
   | otherwise = bodyChanges
+
+parseAmendment ∷ Provenance → [String] → Either [ParseError] Amendment
+parseAmendment sourceProvenance phrases =
+  let citation = findCitation phrases
+      parsedBill = citation >>= makeBill
+      parsedYear = findYear phrases
+      parsedChapter = findChapter phrases
+      parsedEffectiveDate = findEffectiveDate phrases
+      summaryText = findSummary phrases
+      titleChanges = maybe emptyChangeSet findChangedStatutes summaryText
+      bodyChanges = findBodyChangedStatutes phrases
+      errors =
+        concat
+          [ missingError MissingCitation "bill" "Could not find an HB/SB citation" (isNothing citation)
+          , invalidCitationError citation parsedBill
+          , missingError MissingYear "year" "Could not find the Oregon Laws year" (isNothing parsedYear)
+          , missingError MissingChapter "chapter" "Could not find the Oregon Laws chapter" (isNothing parsedChapter)
+          , missingError MissingEffectiveDate "effectiveDate" "Could not parse the effective date" (isNothing parsedEffectiveDate)
+          ]
+  in case (parsedBill, parsedYear, parsedEffectiveDate, parsedChapter) of
+    (Just billValue, Just yearValue, Just effectiveDateValue, Just chapterValue)
+      | null errors -> Right Amendment {
+          bill = billValue,
+          summary = summaryText,
+          affectedSections = selectBestChangeSet titleChanges bodyChanges,
+          year = yearValue,
+          effectiveDate = effectiveDateValue,
+          chapter = chapterValue,
+          validation = reconcileChangeSets titleChanges bodyChanges,
+          provenance = sourceProvenance
+        }
+    _ -> Left errors
+
+missingError ∷ ParseErrorCode → String → String → Bool → [ParseError]
+missingError code field message isMissing
+  | isMissing = [ParseError code (Just field) message]
+  | otherwise = []
+
+invalidCitationError ∷ Maybe String → Maybe Bill → [ParseError]
+invalidCitationError (Just citation) Nothing =
+  [ParseError InvalidCitation (Just "bill") ("Could not parse citation: " ++ citation)]
+invalidCitationError _ _ = []
 
 sectionNumbers ∷ String → [String]
 sectionNumbers phrase =
