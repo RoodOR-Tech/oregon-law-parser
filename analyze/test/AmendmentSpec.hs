9d9ae8d8c4654c4a29f05a65a8178446351680e8
RoodOR-Tech <roodj.or@gmail.com>
2026-08-26 13:35:52 -0700

    Add regression for multiple repeal markers in one section

--- analyze/test/AmendmentSpec.hs
+++ analyze/test/AmendmentSpec.hs
@@ -99,6 +99,10 @@ spec = do
       let ps = ["SECTION 1. (1) ORS 455.612, 455.614, 476.390, 476.394, 477.027, 477.161 and 477.490 and sections 12a, 12b and 29, chapter 592, Oregon Laws 2021, are repealed."]
       findBodyChangedStatutes ps `shouldBe` ChangeSet { amended = [], repealed = ["455.612", "455.614", "476.390", "476.394", "477.027", "477.161", "477.490"] }
 
+    it "chooses the earliest operative marker when a section has multiple repeal subclauses" $ do
+      let ps = ["SECTION 1. (1) ORS 455.612, 455.614, 476.390, 476.394, 477.027, 477.161 and 477.490 and sections 12a, 12b and 29, chapter 592, Oregon Laws 2021, are repealed. (2) Section 12d, chapter 592, Oregon Laws 2021, as amended by section 10, chapter 611, Oregon Laws 2023, is repealed."]
+      findBodyChangedStatutes ps `shouldBe` ChangeSet { amended = [], repealed = ["455.612", "455.614", "476.390", "476.394", "477.027", "477.161", "477.490"] }
+
     it "accepts a conditional becomes-law prefix on a direct ORS amendment" $ do
       let ps = ["SECTION 41. If House Bill 2191 becomes law, ORS 697.612, as amended by section 2, chapter 604, Oregon Laws 2009 (Enrolled House Bill 2191), is amended to read:"]
       findBodyChangedStatutes ps `shouldBe` ChangeSet { amended = ["697.612"], repealed = [] }
